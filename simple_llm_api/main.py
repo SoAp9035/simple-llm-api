daa2131bf85c810ea03ab299f1b3d235bb1a75d5
Ahmet Burhan Kayalı <ahmetburhan1703@gmail.com>
2025-04-23 10:57:51 +0300

    move exceptions to another file

--- simple_llm_api/main.py
+++ simple_llm_api/main.py
@@ -1,25 +1,11 @@
 import requests
-
-
-class OpenAIError(Exception):
-    def __init__(self, message: str = "OpenAIAPI Error"):
-        super().__init__(message)
-
-class AnthropicError(Exception):
-    def __init__(self, message: str = "AnthropicAPI Error"):
-        super().__init__(message)
-
-class GeminiError(Exception):
-    def __init__(self, message: str = "GeminiAPI Error"):
-        super().__init__(message)
-
-class MistralError(Exception):
-    def __init__(self, message: str = "MistralAPI Error"):
-        super().__init__(message)
-
-class DeepSeekError(Exception):
-    def __init__(self, message: str = "DeepSeekAPI Error"):
-        super().__init__(message)
+from .exceptions import (
+    OpenAIError,
+    AnthropicError,
+    GeminiError,
+    MistralError,
+    DeepSeekError
+)
 
 
 class OpenAIAPI:
